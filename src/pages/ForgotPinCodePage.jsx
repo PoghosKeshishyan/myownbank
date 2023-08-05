30971150c9649daebb15925ccb100b28d45869b9
PoghosKeshishyan <poghos877@gmail.com>
2023-08-06 00:38:07 +0400

    commit

--- src/pages/ForgotPinCodePage.jsx
+++ src/pages/ForgotPinCodePage.jsx
@@ -1,24 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../scss/ForgotPinCodePage.scss';
+import axios from '../axios';
 
 export function ForgotPinCode() {
     const [email, setEmail] = useState('');
     const navigate = useNavigate();
 
-    useEffect(() => {
+    const submitHandler = async(e) => {
+        e.preventDefault();
 
-        // const result = localStorage.getItem('myOwnBank_userPinCode');
-        // setPinInLocal(result);
-
-
-    }, [])
+        try {
+            await axios.post('api/auth/forgot/password', { email }, {
+              headers: {
+                'Content-Type': 'application/json',
+              }
+            })
+      
+          } catch (error) {
+            console.log(error);
+          }
+    }
 
     return (
         <div className='ForgotPinCode' data-aos='zoom-in'>
             <i className='fa-solid fa-circle-left' onClick={() => navigate(-1)} style={{top: '20px'}}/>
 
-            <form action='forgotPinCode.php' method='POST'>
+            <form onSubmit={submitHandler}>
                 <p>Forgot your pin code? <br /> Enter your email.</p>
                 <span>We will try to respond to your letter.</span>
 
@@ -31,7 +39,11 @@ export function ForgotPinCode() {
                   onChange={e => setEmail(e.target.value)} 
                 />
  
-                <input type='submit' value='Send' className={email.includes('@') ? ' btn btnActive' : ' btn btnVisible'} />
+                <input 
+                  type='submit' 
+                  value='Send' 
+                  className={email.includes('@') ? ' btn btnActive' : ' btn btnVisible'} 
+                />
             </form>
         </div>
     )
