1e83bd77bf85f31dd3ef0a46e08299aa5cba8f7b
PoghosKeshishyan <poghos877@gmail.com>
2023-09-06 01:23:15 +0400

    commit

--- src/axios.js
+++ src/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export default axios.create({
     // baseURL: 'http://192.168.1.2:3000/',
     // baseURL: 'http://localhost:3000/',
-    baseURL: 'https://tired-newt-tutu.cyclic.app/',
+    baseURL: 'https://good-pink-haddock-belt.cyclic.app/',
 })
